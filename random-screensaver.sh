ebdc0f61078026f6f00a5bdc3713b0061edf4f56
André Souza <an3dree@gmail.com>
2025-11-12 15:44:58 +0000

    add go-life

--- random-screensaver.sh
+++ random-screensaver.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 cmds=(
-    'asciiquarium'
-    'tarts blank'
-    'cbonsai -S --life=60 -w 1'
-    'ttysvr -b 1a1b26 bubbles'
-    'tarts donut'
-    'ttysvr -b 1a1b26 logo dvd'
-    'tarts fire'
-    'gh screensaver -s fireworks'
-    'cmatrix -b -c'
-    'cmatrix -b'
-    'pipes.sh'
-    'tarts matrix'
-    'gh screensaver -s starfield -- --speed 10'
+  'asciiquarium'
+  'tarts blank'
+  'cbonsai -S --life=60 -w 1'
+  'ttysvr -b 1a1b26 bubbles'
+  'tarts donut'
+  'ttysvr -b 1a1b26 logo dvd'
+  'tarts fire'
+  'gh screensaver -s fireworks'
+  'cmatrix -b -c'
+  'cmatrix -b'
+  'pipes.sh'
+  'tarts matrix'
+  'gh screensaver -s starfield -- --speed 10'
+  'go-life'
 )
 
 eval "${cmds[RANDOM % ${#cmds[@]}]}"
